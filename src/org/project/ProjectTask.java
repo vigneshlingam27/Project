907bf55f7c7034b47972ac32c707504b11e700aa
vignehlingam27 <vigneshlingam1@gmail.com>
2022-04-20 19:09:19 +0530

    ammudha code

--- src/org/project/ProjectTask.java
+++ src/org/project/ProjectTask.java
@@ -3,7 +3,7 @@ package org.project;
 public class ProjectTask {
 	public static void main(String[] args) {
 		System.out.println("sample test");
-		System.out.println("final");
+		System.out.println("vicky");
 	}
 
 }
